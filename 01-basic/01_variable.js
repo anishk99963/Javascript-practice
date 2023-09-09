4618ad1a634b265398675012425123e4bb8caf46
Anish Kumar <82306813+anishk99963@users.noreply.github.com>
2023-09-09 19:56:30 +0000

    second

---
+++ 01-basic/01_variable.js
@@ -0,0 +1,10 @@
+const accountId = 124578;
+
+let accountEmail = "anshk98@gmil.com";
+var accountPass = "123456"
+accountCity = "jaipur"
+
+accountEmail = "ansh@gmail.com";
+accountPass = "326598";
+
+console.table([accountEmail, accountId, accountPass, accountCity]);
---
+++ 01-basic/02_dataTypes.js
@@ -0,0 +1,3 @@
+let name = "anish"
+let age = 18
+let isLoggoggedIn = flase
