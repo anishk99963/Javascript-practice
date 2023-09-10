5928a1b0ca919f4c452a702009a472cb28873ad6
Anish Kumar <82306813+anishk99963@users.noreply.github.com>
2023-09-10 10:35:46 +0000

    third commit

---
+++ 01-basic/03_conversionOperator.js
@@ -0,0 +1,20 @@
+let score = 33;
+
+console.log(typeof score)
+console.log(typeof(score))
+
+let valueInNumber = Number(score) 
+
+console.log(typeof valueInNumber);
+
+let isLoggedIn = 1;
+
+let booleanLogged = Boolean(isLoggedIn)
+
+console.log(booleanLogged);
+
+let someNumber = 33
+
+let stringNum = String(someNumber)
+
+console.log(typeof stringNum);
